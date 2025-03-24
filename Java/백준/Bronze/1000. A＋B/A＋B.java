1a13b1bddf5e7a1879df06bb21767c4e60809111
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2025-03-24 22:52:18 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A+B);
+    }
+}
