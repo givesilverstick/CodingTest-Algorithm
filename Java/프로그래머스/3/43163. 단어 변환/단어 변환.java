2c375abb373e5ec3d82525b2625769f562be92a8
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2025-05-18 11:26:31 +0900

    [level 3] Title: 단어 변환, Time: 0.96 ms, Memory: 73.3 MB -BaekjoonHub

--- Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,38 +1,44 @@
 import java.util.*;
 class Solution {
+    
     class WordNode {
         String word;
         int step;
+        
         WordNode(String word, int step) {
             this.word = word;
             this.step = step;
         }
     }
+    
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
         Queue<WordNode> queue = new LinkedList<>();
         Set<String> visited = new HashSet<>();
         
-        queue.add(new WordNode(begin, 0)); // .offer() -> 안전
+        // 첫 시작 단어
+        queue.offer(new WordNode(begin, 0));
         visited.add(begin);
         
         while (!queue.isEmpty()) {
             WordNode node = queue.poll();
+            // 타겟과 같을 때
+            if (node.word.equals(target)) return node.step;
             
-            if (node.word.equals(target)) {
-                return node.step;
-            }
+            // 타겟과 다를 때
             
             for (String w : words) {
-                if (!visited.contains(w) && isOneLetterDifferent(node.word, w)) {
-                    queue.add(new WordNode(w, node.step+1));
+                if (isOneDiff(w, node.word) && !visited.contains(w)) {
+                    System.out.println(w);
+                    queue.offer(new WordNode(w, node.step+1));
                     visited.add(w);
                 }
             }
+            
         }
         return answer;
     }
-    private boolean isOneLetterDifferent(String a, String b) {
+    private boolean isOneDiff(String a, String b) {
         int diff = 0;
         for (int i=0; i<a.length(); i++) {
             if (a.charAt(i) != b.charAt(i)) diff++;
