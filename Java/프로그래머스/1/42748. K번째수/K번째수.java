cb0a351cee5739279f57693a9f643f91d74b6aa6
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2025-09-04 21:15:06 +0900

    [level 1] Title: K번째수, Time: 0.46 ms, Memory: 79.9 MB -BaekjoonHub

--- Java/프로그래머스/1/42748. K번째수/K번째수.java
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,10 +4,11 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         for (int i=0; i<commands.length; i++) {
-            int[] sliced = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(sliced);
-            answer[i] = sliced[commands[i][2]-1];
+            int[] slice = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(slice);
+            answer[i] = slice[commands[i][2]-1];
         }
+        
         return answer;
     }
 }
