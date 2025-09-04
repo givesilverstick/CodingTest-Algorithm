3f2cfeb03618917259a7bfe037b4b4c9e0dece57
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2025-09-04 23:07:10 +0900

    [level 1] Title: 폰켓몬, Time: 5.11 ms, Memory: 84.5 MB -BaekjoonHub

--- Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,17 +1,15 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        Set<Integer> set = new HashSet<>();
-        int len = nums.length;
-        for (int i=0; i< len; i++) {
-            set.add(nums[i]);
+        int answer = nums.length/2;
+        HashSet<Integer> kind = new HashSet<>();
+        
+        for (int num : nums) {
+            kind.add(num);
         }
-        if (set.size() > len/2) {
-            answer = len/2;
-        } else {
-            answer = set.size();
-        }
+        
+        if (kind.size() < nums.length/2) answer = kind.size();
         return answer;
     }
 }
