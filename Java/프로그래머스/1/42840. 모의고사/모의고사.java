cad2bd3d20b85f16b8bb25b0b8fcef328372eaf1
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2025-09-05 21:10:38 +0900

    [level 1] Title: 모의고사, Time: 4.14 ms, Memory: 76.4 MB -BaekjoonHub

--- Java/프로그래머스/1/42840. 모의고사/모의고사.java
+++ Java/프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,36 +1,27 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] pattern_1 = {1, 2, 3, 4, 5};
-        int[] pattern_2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] pattern_3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] scores = new int[3];
+        int[] answer = {};
+        int[] score = new int[3];
+        int[] pattern_stu1 = {1,2,3,4,5};
+        int[] pattern_stu2 = {2,1,2,3,2,4,2,5};
+        int[] pattern_stu3 = {3,3,1,1,2,2,4,4,5,5};
         
         for (int i=0; i<answers.length; i++) {
-            if (answers[i] == pattern_1[i%pattern_1.length]) {
-                scores[0]++;
-            }
-            if (answers[i] == pattern_2[i%pattern_2.length]) {
-                scores[1]++;
-            }
-            if (answers[i] == pattern_3[i%pattern_3.length]) {
-                scores[2]++;
-            }
-        }
-        int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
-        
-        List<Integer> result = new ArrayList<>();
-        for (int i=0; i<3; i++) {
-            if (scores[i]==max) {
-                result.add(i+1);
-            }
+            if (answers[i] == pattern_stu1[i%pattern_stu1.length]) score[0]++;
+            if (answers[i] == pattern_stu2[i%pattern_stu2.length]) score[1]++;
+            if (answers[i] == pattern_stu3[i%pattern_stu3.length]) score[2]++;
         }
         
-        int[] answer = new int[result.size()];
-        for (int i=0; i<result.size(); i++) {
-            answer[i] = result.get(i);
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        
+        List<Integer> answerList = new ArrayList<>();
+        for (int i=0; i<score.length; i++) {
+            if (score[i]==maxScore) answerList.add(i+1);
         }
+        System.out.println(answerList);
+        answer = answerList.stream().mapToInt(i -> i).toArray();
         return answer;
     }
 }
