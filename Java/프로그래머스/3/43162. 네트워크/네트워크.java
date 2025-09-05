265de2a52d2e351078de965bb8a68022a89c7b12
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2025-09-06 00:51:05 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 78.5 MB -BaekjoonHub

--- Java/프로그래머스/3/43162. 네트워크/네트워크.java
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,21 +2,20 @@ class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
         boolean[] visited = new boolean[n];
-        
         for (int i=0; i<n; i++) {
             if (!visited[i]) {
-                dfs(computers, visited, i, n);
+                dfs(computers, visited, i);
                 answer++;
             }
         }
         return answer;
     }
-    private void dfs(int[][] computers, boolean[] visited, int current, int n) {
-        visited[current] = true;
-        
-        for (int i=0; i<n; i++) {
-            if (i!=current && !visited[i] && computers[current][i]==1) {
-                dfs(computers, visited, i, n);
+    
+    void dfs(int[][] computers, boolean[] visited, int depth) {
+        visited[depth] = true;
+        for (int i=0; i<computers.length; i++) {
+            if (!visited[i] && computers[i][depth]==1) {
+                dfs(computers, visited, i);
             }
         }
     }
