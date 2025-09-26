9c2c0b7d1dc79826425f4d60e13dcad209d74dd6
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2025-09-27 04:32:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.64 ms, Memory: 97.8 MB -BaekjoonHub

--- Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,23 +3,15 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        
-        // 중복 고려X, HashMap 사용 못해서 틀림
         Map<String, Integer> map = new HashMap<>();
-        
-        // 참가자 이름 맵에 카운트
-        for (String name : participant) {
-            map.put(name, map.getOrDefault(name, 0) + 1);
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p,0)+1);
         }
-        
-        // 완주한 사람 이름 카운트 줄이기 -> 미완주한 사람을 찾아야 함.
-        for (String name : completion) {
-            map.put(name, map.get(name) - 1);
+        for (String c : completion) {
+            map.put(c, map.get(c)-1);
         }
-        
-        // 값이 1인 사람이 완주 못 한 사람
         for (String key : map.keySet()) {
-            if (map.get(key) > 0) {
+            if(map.get(key) == 1) {
                 answer = key;
             }
         }
