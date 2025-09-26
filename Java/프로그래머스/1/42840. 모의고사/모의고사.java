49d2216bbc702d5bccde3c3a8cd13856a4e230a9
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2025-09-27 06:40:31 +0900

    [level 1] Title: 모의고사, Time: 6.68 ms, Memory: 86.5 MB -BaekjoonHub

--- Java/프로그래머스/1/42840. 모의고사/모의고사.java
+++ Java/프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,27 +1,29 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
         int[] answer = {};
+        List<Integer> answerList = new ArrayList<>();
         int[] score = new int[3];
-        int[] pattern_stu1 = {1,2,3,4,5};
-        int[] pattern_stu2 = {2,1,2,3,2,4,2,5};
-        int[] pattern_stu3 = {3,3,1,1,2,2,4,4,5,5};
+        // pattern
+        int[] p1 = {1,2,3,4,5};
+        int[] p2 = {2,1,2,3,2,4,2,5};
+        int[] p3 = {3,3,1,1,2,2,4,4,5,5};
         
         for (int i=0; i<answers.length; i++) {
-            if (answers[i] == pattern_stu1[i%pattern_stu1.length]) score[0]++;
-            if (answers[i] == pattern_stu2[i%pattern_stu2.length]) score[1]++;
-            if (answers[i] == pattern_stu3[i%pattern_stu3.length]) score[2]++;
+            if (answers[i] == p1[i%p1.length]) score[0]++;
+            if (answers[i] == p2[i%p2.length]) score[1]++;
+            if (answers[i] == p3[i%p3.length]) score[2]++;
         }
         
-        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
-        
-        List<Integer> answerList = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
         for (int i=0; i<score.length; i++) {
-            if (score[i]==maxScore) answerList.add(i+1);
+            if (score[i] > max) max = score[i];
         }
-        System.out.println(answerList);
-        answer = answerList.stream().mapToInt(i -> i).toArray();
+        
+        for (int i=0; i<score.length; i++) {
+            if (score[i] == max) answerList.add(i+1);
+        }
+        answer = answerList.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
