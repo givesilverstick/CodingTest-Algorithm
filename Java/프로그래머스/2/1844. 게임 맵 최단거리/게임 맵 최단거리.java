23008e5e70d961edc80d8ade713cc074cbfbc37e
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2025-11-11 18:12:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.29 ms, Memory: 56.8 MB -BaekjoonHub

--- Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,32 +3,35 @@ import java.util.*;
 class Solution {
     int[] dx = {-1, 1, 0, 0};
     int[] dy = {0, 0, -1, 1};
+    
     public int solution(int[][] maps) {
         int answer = -1;
-        int n = maps.length;
-        int m = maps[0].length;
+        int n = maps.length, m = maps[0].length;
         boolean[][] visited = new boolean[n][m];
         int[][] distance = new int[n][m];
         
-        Queue<int[]> queue = new LinkedList<>();
-        
-        queue.add(new int[] {0,0});
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {0,0});
         visited[0][0] = true;
         distance[0][0] = 1;
         
-        while(!queue.isEmpty()) {
-            int[] cur = queue.poll();
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
             for (int i=0; i<dx.length; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
                 if (nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny] && maps[nx][ny]==1) {
-                    queue.add(new int[] {nx, ny});
+                    q.add(new int[] {nx,ny});
                     visited[nx][ny] = true;
                     distance[nx][ny] = distance[cur[0]][cur[1]]+1;
                 }
             }
         }
-        if(visited[n-1][m-1]) answer = distance[n-1][m-1];
+        if (visited[n-1][m-1]) answer = distance[n-1][m-1];
+        
         return answer;
+    }
+    void bfs() {
+        
     }
 }
