a79707416aeb1543a28e77cce595083d31decc60
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2025-11-11 16:27:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.92 ms, Memory: 98.7 MB -BaekjoonHub

--- Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,16 +4,22 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Map<String, Integer> map = new HashMap<>();
+        
         for (String p : participant) {
             map.put(p, map.getOrDefault(p,0)+1);
         }
+        
         for (String c : completion) {
-            map.put(c, map.get(c)-1);
-        }
-        for (String key : map.keySet()) {
-            if(map.get(key) == 1) {
-                answer = key;
+            if (map.get(c) > 1) {
+                map.put(c, map.get(c)-1);
+            } else {
+                map.remove(c);
             }
+            
+        }
+        
+        for (String key : map.keySet()) {
+            answer = key;
         }
         return answer;
     }
