2aafaba1f84bf68c8430c736dbd5e0d6e9d587cb
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2026-03-11 14:25:00 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,174 ms, Memory: 122,644 KB -BaekjoonHub

--- Java/SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ Java/SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,25 +2,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int V, E, parents[];
+	static class Node {
+		int vertex, weight;
+		Node next;
+		
+		Node(int vertex, int weight, Node next) {
+			this.vertex = vertex;
+			this.weight = weight;
+			this.next = next;
+		}
+	}
 	
-	static class Edge implements Comparable<Edge> {
-		int start, end, weight;
-		Edge(int start, int end, int weight) {
-			this.start = start;
-			this.end = end;
+	static class Vertex implements Comparable<Vertex> {
+		int vertex, weight;
+		
+		Vertex(int vertex, int weight) {
+			this.vertex = vertex;
 			this.weight = weight;
 		}
 		
 		@Override
-		public int compareTo(Edge o) {
+		public int compareTo(Vertex o) {
 			return Integer.compare(this.weight, o.weight);
 		}
 	}
+	
+	static int V, E, parents[];
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,57 +43,44 @@ public class Solution {
 			st = new StringTokenizer(br.readLine());
 			V = Integer.parseInt(st.nextToken());	// 정점 개수
 			E = Integer.parseInt(st.nextToken());	// 간선 개수
-			Edge[] edgeList = new Edge[E];
-			makeSets();
+			Node[] adjList = new Node[V];
+			boolean[] visited = new boolean[V];
+			int[] minEdge = new int[V];
+			
 			for (int i=0; i<E; i++) {
 				st = new StringTokenizer(br.readLine());
 				int A = Integer.parseInt(st.nextToken());
 				int B = Integer.parseInt(st.nextToken());
 				int C = Integer.parseInt(st.nextToken());	// 가중치
-				edgeList[i] = new Edge(A,B,C);
+				adjList[A-1] = new Node(B-1, C, adjList[A-1]);
+				adjList[B-1] = new Node(A-1, C, adjList[B-1]);
 			}
 			
-			Arrays.sort(edgeList);
+			Arrays.fill(minEdge, Integer.MAX_VALUE);
+			minEdge[0] = 0;
 			
-			int count = 0;
 			long result = 0;
+			PriorityQueue<Vertex> pq = new PriorityQueue<>();
+			pq.offer(new Vertex(0, 0));	// minVertex, min
 			
-			for (Edge edge : edgeList) {
-				if (union(edge.start, edge.end)) {
-					result += edge.weight;
-					if (++count==V-1) break;
+			while(!pq.isEmpty()) {
+				Vertex cur = pq.poll();
+				
+				if (visited[cur.vertex]) continue;
+				
+				visited[cur.vertex] = true;
+				result += cur.weight;
+				
+				for (Node temp=adjList[cur.vertex]; temp!=null; temp=temp.next) {
+					if (!visited[temp.vertex] && minEdge[temp.vertex]>temp.weight) {
+						minEdge[temp.vertex] = temp.weight;
+						pq.offer(new Vertex(temp.vertex, minEdge[temp.vertex]));
+					}
 				}
+					
 			}
-			
 			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
 		System.out.println(sb);
-	}
-	
-	static void makeSets() {
-		parents = new int[V+1];
-		for (int i=1; i<=V; i++) {
-			parents[i] = -1;
-		}
-	}
-	
-	static int findSet(int a) {
-		if (parents[a]<0) return a;
-		return parents[a] = findSet(parents[a]);
-	}
-	
-	static boolean union(int a, int b) {
-		int aRoot = findSet(a);
-		int bRoot = findSet(b);
-		if (aRoot==bRoot) return false;
-		
-		if (parents[aRoot]<=parents[bRoot]) {
-			parents[aRoot] += parents[bRoot];
-			parents[bRoot] = aRoot;
-		} else {
-			parents[bRoot] += parents[aRoot];
-			parents[aRoot] = bRoot;
-		}
-		return true;
 	}
 }
