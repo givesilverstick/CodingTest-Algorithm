e02e58c4e8576a7f692fddc0d1cd9a606c58a96b
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2026-03-12 12:18:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 90 ms, Memory: 24,320 KB -BaekjoonHub

---
+++ Java/SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int tc=1; tc<=T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+			String str = br.readLine();
+			char[] nums = new char[N];
+			for (int i=0; i<N; i++) {
+				nums[i] = str.charAt(i);
+			}
+			
+			Set<String> set = new HashSet<>();
+			for (int i=0; i<N/4; i++) {
+				for (int j=0; j<4; j++) {
+					set.add(str.substring(j*N/4, (j+1)*N/4));
+				}
+				str = str.charAt(str.length()-1) + str.substring(0,str.length()-1);
+			}
+			String[] arr = set.toArray(new String[0]);
+			Arrays.sort(arr, Collections.reverseOrder());
+			sb.append("#").append(tc).append(" ").append(Integer.parseInt(arr[K-1], 16)).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
