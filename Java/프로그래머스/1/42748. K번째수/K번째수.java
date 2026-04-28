2ac7624ad1dff8e62c89a738bf14815c84998291
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2026-04-29 02:32:01 +0900

    [level 1] Title: K번째수, Time: 1.26 ms, Memory: 78.4 MB -BaekjoonHub

--- Java/프로그래머스/1/42748. K번째수/K번째수.java
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,10 +3,14 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        for (int i=0; i<commands.length; i++) {
-            int[] slice = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(slice);
-            answer[i] = slice[commands[i][2]-1];
+        int idx = 0;
+        for (int[] cmd : commands) {
+            int i = cmd[0];
+            int j = cmd[1];
+            int k = cmd[2];
+            int[] sliceArr = Arrays.copyOfRange(array, i-1, j);
+            Arrays.sort(sliceArr);
+            answer[idx++] = sliceArr[k-1];
         }
         return answer;
     }
--- Java/프로그래머스/1/42748. K번째수/README.md
+++ Java/프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 83.6 MB, 시간: 0.45 ms
+메모리: 78.4 MB, 시간: 1.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 27일 04:00:00
+2026년 04월 29일 02:31:57
 
 ### 문제 설명
 
