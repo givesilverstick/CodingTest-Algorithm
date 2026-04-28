af90570b7bed370851bea9498f2cc7ff12cab3ed
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2026-04-29 03:32:18 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 89 MB -BaekjoonHub

--- Java/프로그래머스/2/42578. 의상/의상.java
+++ Java/프로그래머스/2/42578. 의상/의상.java
@@ -4,15 +4,15 @@ class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
         Map<String, Integer> map = new HashMap<>();
-        
-        for (String[] cloth : clothes) {
-            map.put(cloth[1], map.getOrDefault(cloth[1],0) + 1);
+        for (String[] c : clothes) {
+            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
         }
         
-        for (String key : map.keySet()) {
-            answer *= map.get(key)+1;
+        Set<String> key = map.keySet();
+        if (key.size()==1) return clothes.length;
+        for (String s : map.keySet()) {
+            answer *= map.get(s)+1;
         }
-        
         return answer-1;
     }
 }
