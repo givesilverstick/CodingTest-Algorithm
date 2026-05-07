1b01dc745073bf3a7015929485c3f791c9a72869
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2026-05-08 01:00:05 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 77.2 MB -BaekjoonHub

--- Java/프로그래머스/3/43162. 네트워크/네트워크.java
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,26 @@
 class Solution {
+    static int N, comps[][];
     static boolean[] checked;
-    static int N, adj[][];
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        checked = new boolean[n];
         N = n;
-        adj = computers;
+        comps = computers;
+        checked = new boolean[n];
+        
         for (int i=0; i<n; i++) {
             if (!checked[i]) {
-                search(i);
+                dfs(i);
                 answer++;
             }
         }
         return answer;
     }
     
-    void search(int idx) {
+    void dfs(int idx) {
         checked[idx] = true;
+        
         for (int i=0; i<N; i++) {
-            if (idx!=i && !checked[i] && adj[idx][i]==1) {
-                search(i);
-            }
+            if (idx!=i && !checked[i] && comps[idx][i]==1) dfs(i);
         }
     }
 }
