2b9b2adaf9a0b954023e6bb76e461b920841c765
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2026-05-08 00:53:46 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.10 ms, Memory: 58 MB -BaekjoonHub

--- Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,37 +1,43 @@
 import java.util.*;
 
 class Solution {
-    int[] dx = {-1, 1, 0, 0};
-    int[] dy = {0, 0, -1, 1};
-    
+    // dfs는 효율성 테스트에서 시간초과.
+    static final int INF = Integer.MAX_VALUE;
+    static int[] dr={-1,1,0,0}, dc={0,0,-1,1};
+    static int answer, Map[][], n, m;
+    static boolean[][] visited;
     public int solution(int[][] maps) {
-        int answer = -1;
-        int n = maps.length, m = maps[0].length;
-        boolean[][] visited = new boolean[n][m];
-        int[][] distance = new int[n][m];
-        
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {0,0});
+        answer = INF;
+        Map = maps;
+        n = Map.length;
+        m = Map[0].length;
+        visited = new boolean[n][m];
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {0,0, 1});    // r,c cnt
         visited[0][0] = true;
-        distance[0][0] = 1;
         
         while(!q.isEmpty()) {
             int[] cur = q.poll();
-            for (int i=0; i<dx.length; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
-                if (nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny] && maps[nx][ny]==1) {
-                    q.add(new int[] {nx,ny});
-                    visited[nx][ny] = true;
-                    distance[nx][ny] = distance[cur[0]][cur[1]]+1;
+            int r = cur[0];
+            int c = cur[1];
+            int cnt = cur[2];
+            
+            if (r==n-1 && c==m-1) {
+                answer = Math.min(answer, cnt);
+                break;
+            }
+            
+            for (int d=0; d<4; d++) {
+                int nr = r+dr[d];
+                int nc = c+dc[d];
+                if (nr<0 || nr>=n || nc<0 || nc>=m || visited[nr][nc]) continue;
+                if (maps[nr][nc]==1) {
+                    q.offer(new int[] {nr,nc, cnt+1});
+                    visited[nr][nc] = true;
                 }
             }
+            
         }
-        if (visited[n-1][m-1]) answer = distance[n-1][m-1];
-        
-        return answer;
-    }
-    void bfs() {
-        
+        return answer==INF ? -1 : answer;
     }
 }
