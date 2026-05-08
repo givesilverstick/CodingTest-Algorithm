2088c43a90c507f5850b8d18ccdfd2395694f2ef
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2026-05-08 17:58:31 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ MySQL/프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,5 +1,8 @@
-SELECT i.name, i.datetime from animal_ins i
-left join animal_outs o on i.animal_id = o.animal_id
-where o.datetime is null
-order by i.datetime
-limit 3;
+-- 코드를 입력하세요
+SELECT i.NAME, i.DATETIME
+FROM ANIMAL_INS i
+LEFT JOIN ANIMAL_OUTS o ON i.ANIMAL_ID = o.ANIMAL_ID
+WHERE o.DATETIME IS NULL
+ORDER BY i.DATETIME ASC
+LIMIT 3
+;
