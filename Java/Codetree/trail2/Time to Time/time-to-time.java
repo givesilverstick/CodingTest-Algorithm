930dc1faedb8afa896a437308a7c4cd4dc403555
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2026-05-12 17:07:29 +0900

    [Passed] Time to Time (89ms, 9MB)

---
+++ Java/Codetree/trail2/Time to Time/time-to-time.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        System.out.println((C*60+D) - (A*60+B));
+    }
+}
