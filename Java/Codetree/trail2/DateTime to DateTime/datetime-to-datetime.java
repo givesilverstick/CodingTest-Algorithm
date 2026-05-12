4f268aaabcb7c8ded14090bab4b8f28e3269a075
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2026-05-12 17:38:32 +0900

    [Passed] DateTime to DateTime (119ms, 10MB)

---
+++ Java/Codetree/trail2/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        // Please write your code here.
+        int fix = 11*24*60 + 11*60 + 11;
+        int compare = A*24*60 + B*60 + C;
+        if (fix > compare) System.out.println(-1);
+        else System.out.println(compare-fix);
+    }
+}
