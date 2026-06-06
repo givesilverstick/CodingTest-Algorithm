53fbb07bb2319a2435d8e6b2cdaa638980085a92
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2026-06-06 23:27:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.13 ms, Memory: 58.1 MB -BaekjoonHub

--- Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,43 +1,36 @@
 import java.util.*;
 
 class Solution {
-    // dfs는 효율성 테스트에서 시간초과.
-    static final int INF = Integer.MAX_VALUE;
-    static int[] dr={-1,1,0,0}, dc={0,0,-1,1};
-    static int answer, Map[][], n, m;
-    static boolean[][] visited;
+    static int[] dr = {-1,1,0,0}, dc = {0,0,-1,1};
     public int solution(int[][] maps) {
-        answer = INF;
-        Map = maps;
-        n = Map.length;
-        m = Map[0].length;
-        visited = new boolean[n][m];
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[] {0,0, 1});    // r,c cnt
+        int answer = -1; // 최단 거리
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        Queue<int[]> q = new ArrayDeque<>(); // r,c, count
+        boolean[][] visited = new boolean[n][m];
+        
+        q.offer(new int[] {0,0, 1});
         visited[0][0] = true;
         
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int[] cur = q.poll();
-            int r = cur[0];
-            int c = cur[1];
-            int cnt = cur[2];
-            
-            if (r==n-1 && c==m-1) {
-                answer = Math.min(answer, cnt);
-                break;
+            if (cur[0]==n-1 && cur[1]==m-1) {
+                return cur[2];
             }
-            
             for (int d=0; d<4; d++) {
-                int nr = r+dr[d];
-                int nc = c+dc[d];
+                int nr = cur[0]+dr[d];
+                int nc = cur[1]+dc[d];
+                
                 if (nr<0 || nr>=n || nc<0 || nc>=m || visited[nr][nc]) continue;
+                
                 if (maps[nr][nc]==1) {
-                    q.offer(new int[] {nr,nc, cnt+1});
+                    q.offer(new int[] {nr,nc, cur[2]+1});
                     visited[nr][nc] = true;
                 }
             }
-            
         }
-        return answer==INF ? -1 : answer;
+        
+        return answer;
     }
 }
