a7357ff7c2f15e8f0d8d694304140a0e196f6438
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2026-06-06 22:59:55 +0900

    [level 2] Title: 프로세스, Time: 1.30 ms, Memory: 65.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<Integer> runQ = new ArrayDeque<>();   // 실행 대기 큐
+        Queue<Integer> idxQ = new ArrayDeque<>();   // 인덱스 저장
+        int maxPrior = 0;
+        for (int i=0; i<priorities.length; i++) {
+            runQ.offer(priorities[i]);
+            idxQ.offer(i);
+            maxPrior = Math.max(maxPrior, priorities[i]);
+        }
+        
+        List<Integer> order = new ArrayList<>();    // 실행 순서대로 인덱스 저장
+        while (!runQ.isEmpty()) {
+            int cur = runQ.poll();
+            int idx = idxQ.poll();
+            boolean isPrior = true;
+            for (int i=cur+1; i<=maxPrior; i++) {
+                if (runQ.contains(i)) {
+                    isPrior = false;
+                    break;
+                }
+            }
+            if (isPrior) {
+                order.add(idx);
+            } else {
+                runQ.offer(cur);
+                idxQ.offer(idx);
+            }
+        }
+        int answer = order.indexOf(location)+1;
+        return answer;
+    }
+}
