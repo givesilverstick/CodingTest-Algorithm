da078e7016faa0489ae56a3bb6c704830847fcff
Eunchae Cho <108306340+givesilverstick@users.noreply.github.com>
2026-06-06 23:15:35 +0900

    [level 2] Title: 타겟 넘버, Time: 2.96 ms, Memory: 63.5 MB -BaekjoonHub

--- Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,19 @@
 class Solution {
-    static int nums[], answer, t;
+    static int answer = 0; // 타겟 넘버를 만드는 방법의 수
     public int solution(int[] numbers, int target) {
-        answer = 0;
-        nums = numbers;
-        t = target;
-        dfs(0,0);
+        
+        dfs(numbers, target, 0, 0);
+        
         return answer;
     }
-    void dfs(int cnt, int sum) {
-        if (cnt==nums.length) {
-            if (sum == t) answer++;
+    
+    public void dfs(int[] numbers, int target, int idx, int sum) {
+        if (idx == numbers.length) {
+            if (sum == target) answer++;
             return;
         }
         
-        dfs(cnt+1, sum+nums[cnt]);
-        dfs(cnt+1, sum-nums[cnt]);
+        dfs(numbers, target, idx+1, sum+numbers[idx]);  // 더하기
+        dfs(numbers, target, idx+1, sum-numbers[idx]);  // 빼기
     }
 }
